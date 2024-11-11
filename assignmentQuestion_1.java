470a7d262fa13f1faf3054bd4878e683b1115436
ankit87Gupta302 <ag9497436@gmail.com>
2024-11-11 15:14:12 +0530

    upload first question of assignment

---
+++ assignmentQuestion_1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class assignmentQuestion_1{
+        public static void main(String args[]) {
+
+        // Question number -07
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of n");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        
+
+        
+    }
+}
